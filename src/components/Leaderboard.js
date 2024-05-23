7c697b85338100a36942ff4cba23a2fead1d14d2
uma656 <qwertyuncle555@gmail.com>
2024-05-23 18:56:54 +0500

    Tracking fitness work

---
+++ src/components/Leaderboard.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './Leaderboard.css';
+
+function Leaderboard({ leaderboardData }) {
+    return (
+        <div>
+            <h2>User's Record</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Rank</th>
+                        <th>Name</th>
+                        <th>Exercise</th>
+                        <th>Score</th>
+                        <th>Diet Plan</th>
+                        <th>Goal</th>
+                        <th>Steps</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {leaderboardData.map((participant, index) => (
+                        <tr key={index}>
+                            <td>{index + 1}</td>
+                            <td>{participant.name}</td>
+                            <td>{participant.exercise}</td>
+                            <td>{participant.score}</td>
+                            <td>{participant.diet}</td>
+                            <td>{participant.goal}</td>
+                            <td>{participant.steps}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default Leaderboard;
